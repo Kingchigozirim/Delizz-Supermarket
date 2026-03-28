85048c1e9375e723179b1962518549881c69d53b
unknown <kingchikajosh@gmail.com>
2026-03-28 14:25:57 +0100

    delizz

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,12 +1,15 @@
 const categories = [
   { id: "groceries", name: "Groceries" },
-  { id: "alcoholic-drinks", name: "Alcoholic Drinks" },
-  { id: "non-alcoholic-drinks", name: "Non Alcoholic Drinks" },
+  { id: "wine", name: "Wine Region" },
+  { id: "non-alcoholic-drinks", name: "Soft Drinks" },
   { id: "cereals", name: "Cereals" },
   { id: "health-beauty", name: "Health & Beauty" },
   { id: "cosmetics", name: "Cosmetics" },
   { id: "electronics", name: "Electronics" },
   { id: "home-kitchen", name: "Home & Kitchen" },
+  { id: "beer", name: "Beer" },
+  { id: "frozen-foods", name: "Frozen Foods" },
+
 ]
 
 export default categories
